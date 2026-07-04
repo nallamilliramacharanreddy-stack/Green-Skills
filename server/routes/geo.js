0ff70dbd90d6e6cb3ceb2286b7dff53bef07b701
NALLAMILLI RAMA CHARAN <nallamilliramacharanreddygmail.com@NALLAMILLIs-MacBook-Air.local>
2026-07-04 10:27:05 +0530

    fix: Correct auth middleware function name to protect in geo routes

--- server/routes/geo.js
+++ server/routes/geo.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { auth } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/authMiddleware');
 const { geocodePincode, updateUserLocation } = require('../controllers/geoController');
 
 // GET /api/geo/geocode?pincode=123456
-router.get('/geocode', auth, geocodePincode);
+router.get('/geocode', protect, geocodePincode);
 
 // POST /api/geo/location
-router.post('/location', auth, updateUserLocation);
+router.post('/location', protect, updateUserLocation);
 
 module.exports = router;
