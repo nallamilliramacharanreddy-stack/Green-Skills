b39f130d390472ee4d9eb97086ce039ccbf69988
NALLAMILLI RAMA CHARAN <nallamilliramacharanreddygmail.com@NALLAMILLIs-MacBook-Air.local>
2026-07-04 11:08:29 +0530

    fix: Update geoAPI to use centralized API_URL to fix localhost fetching on production

--- client/src/api/geoAPI.js
+++ client/src/api/geoAPI.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
+import { API_URL } from '../utils/api';
 
 const geoAPI = {
   geocodePincode: async (pincode, country = 'in') => {
