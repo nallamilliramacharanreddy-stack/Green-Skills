367819422a4fedbf27a3f77f31537d19ff1a22e0
NALLAMILLI RAMA CHARAN <nallamilliramacharanreddygmail.com@NALLAMILLIs-MacBook-Air.local>
2026-07-04 10:24:24 +0530

    fix: Correct auth middleware import path in geo routes

--- server/routes/geo.js
+++ server/routes/geo.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { auth } = require('../middleware/auth');
+const { auth } = require('../middleware/authMiddleware');
 const { geocodePincode, updateUserLocation } = require('../controllers/geoController');
 
 // GET /api/geo/geocode?pincode=123456
